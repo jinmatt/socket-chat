4ec8eeff8505a8177e8ce7b7fec3db7b6ed1caba
Jinsu Mathew <jinsu@mindhelix.com>
2014-05-27 23:18:55 +0530

    Some code refactoring done. Readme updated.

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 Using Socket.io and Express.js(app without the express generator templates).
 
+## Run the example app
+
+```
+$ cd socket-chat
+$ npm install
+$ npm start
+```
+
+The app with start in port `3000` in you local machine.
+
+Append /nickname to display your nick name in chat room. e.g. Open the app in brower as `http://localhost:3000/john`. Open multiple windows with different nickname to see the magic of sockets!
+
 \- jinmatt
--- server.js
+++ server.js
@@ -2,31 +2,49 @@ var express = require('express'),
     http = require('http');
 
 var app = express();
-var server = http.createServer(app);
-server.listen(3000);
+var server = http.createServer(app).listen(3000);
 var io = require('socket.io').listen(server);
 
 var chat_room = io;
 
-var username;
+var username; // a temp variable to collect user's nickname from GET /
+
+// A reusable User object to whole unique user nicknames
 function User(name) {
   this.name = name;
 }
 
+/**
+ * Chat room view
+ *
+ * if / is called, user will be Anonymous
+ * if /nickname is passed, we can use it to identify user
+ */
 app.get('/*', function(req, res) {
-  if (req.params[0])
+  if (req.params[0]) // if nickname is passed
     username = req.params[0];
   else
     username = 'Anonymous';
+
+  // show the chat room page
   res.sendfile(__dirname + '/public/index.html');
 });
 
+
+/**
+ * chat room socket operations
+ */
 chat_room.sockets.on('connection', function(socket) {
+
+  // new user with nickname created
   var user = new User(username);
 
+  // Welcome message on entrance event
   var welcome_msg = 'Welcome to chat room ' + user.name;
   if (user.name == 'Anonymous')
-    welcome_msg = 'Welcome to chat room. Append /yourname to display your name.';
+    welcome_msg = 'Welcome to chat room. Append /nickname with url to display it with your chat.';
+
+  // Socket events
   socket.emit('entrance', { message: welcome_msg });
 
   socket.on('disconnect', function() {
